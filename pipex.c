713908e90da0cff02eadbb9935868312b28c0862
Emilie Minatchy <emilieminatchy@MacBook-Air-de-Emilie.local>
2022-10-11 00:35:41 +0200

    11.10

--- pipex.c
+++ pipex.c
@@ -10,17 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void    pipex()
+void	child_process(char **argv, char **envp, int *fd)
 {
-    int fd[2];
-    pid_t   parent;
-    
-    pipe(fd);
-    parent = fork();
-    if (parent < 0)
-        return (perror("Fork: error"));
-    if (!parent)
-        child_process(f1, cmd1);
-    else
-        parent_proess(f2, cmd2);
+	int		filein;
+
+	filein = open(argv[1], O_RDONLY);
+	if (filein == -1)
+		error();
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filein, STDIN_FILENO);
+	close(fd[0]);
+	execute(argv[2], envp);
+}
+
+void	parent_process(char **argv, char **envp, int *fd)
+{
+	int		fileout;
+
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fileout == -1)
+		error();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fileout, STDOUT_FILENO);
+	close(fd[1]);
+	execute(argv[3], envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	fd[2];
+	int	pid;
+
+	while (argc)
+	{
+		if (pipe(fd) == -1)
+			error();
+		pid = fork();
+		if (pid == -1)
+			error();
+		if (pid == 0)
+			child_process(argv, envp, fd);
+		waitpid(pid, NULL, 0); // null pour opt
+		parent_process(argv, envp, fd);
+	}
+	return (0);
 }
